e51bb089e0d5f80b36571caa5404ff10c11e3371
NotFiege <91145413+NotFiege@users.noreply.github.com>
2024-02-01 13:27:10 -0600

    Create initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,14 @@
+// Fun fact about myself; I've visited most of the United States
+
+#include <iostream>
+
+int main()
+{
+
+  std::cout << "EEEEEE FFFFFF\n";
+  std::cout << "E      F     \n";
+  std::cout << "EEEEEE FFFFFF\n";
+  std::cout << "E      F     \n";
+  std::cout << "EEEEEE F     \n";
+
+}
