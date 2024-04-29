ed2148e2b8c5ce2fc1a1128822b9852d593dd724
NotFiege <91145413+NotFiege@users.noreply.github.com>
2024-04-29 15:45:58 -0500

    Create initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,12 @@
+// I have a pet rabbit named Kiko
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "EEEEEEEE  FFFFFFFF\n";
+  std::cout << "E         F       \n";
+  std::cout << "EEEEEEEE  FFFFFFFF\n";
+  std::cout << "E         F       \n";
+  std::cout << "EEEEEEEE  F       \n";
+}
