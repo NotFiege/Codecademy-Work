9cfc100984da9e0ec0dbf72860609b2992f217f4
NotFiege <91145413+NotFiege@users.noreply.github.com>
2024-04-29 15:00:03 -0500

    Delete letter.cpp

--- letter.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() 
-{
-  
-  std::cout << "Dear Self,\n";
-  std::cout << "If you're reading this, you're one step closer to reaching success. Don't forget what happened in Jan 2024 and use that as fuel to change your life.\n";
-  
-}
