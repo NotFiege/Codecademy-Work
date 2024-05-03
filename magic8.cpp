f5b2f95b3d7496189504f2626dcab049e29edf30
NotFiege <91145413+NotFiege@users.noreply.github.com>
2024-05-03 12:35:27 -0500

    Create magic8.cpp

---
+++ magic8.cpp
@@ -0,0 +1,69 @@
+// Magic 8 Ball generator
+
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+
+  srand(time(NULL));
+  int answer = std::rand() % 10;
+
+  std::cout << "MAGIC 8 BALL\n\n";
+
+  if (answer == 0)
+  {
+    std::cout << "It is certain.\n";
+  }
+
+  else if (answer == 1)
+  {
+    std::cout << "Without a doubt.\n";
+  }
+
+  else if (answer == 2)
+  {
+    std::cout << "Most likely.\n";
+  }
+
+  else if (answer == 3)
+  {
+    std::cout << "Yes.\n";
+  }
+
+  else if (answer == 4)
+  {
+    std::cout << "Reply hazy, try again.\n";
+  }
+
+  else if (answer == 5)
+  {
+    std::cout << "Ask again later.\n";
+  }
+
+  else if (answer == 6)
+  {
+    std::cout << "Concentrate and ask again.\n";
+  }
+
+  else if (answer == 7)
+  {
+    std::cout << "Don't count on it.\n";
+  }
+
+  else if (answer == 8)
+  {
+    std::cout << "My reply is no.\n";
+  }
+
+  else if (answer == 9)
+  {
+    std::cout << "My sources say no.\n";
+  }        
+
+  else
+  {
+    std::cout << "Very doubtful.\n";
+  }
+
+}
