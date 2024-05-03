d72fc962bd3039d88ef557ab990858783e151b93
NotFiege <91145413+NotFiege@users.noreply.github.com>
2024-05-03 12:04:51 -0500

    Create not.cpp

---
+++ not.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() 
+{
+  
+  bool logged_in = false;
+  
+  // Write the code below:
+  
+  if (logged_in == !true)
+  {
+    std::cout << "Try again.\n";
+  }
+  
+}
