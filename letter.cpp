7fa6e4da0bcc4337944f383ef7056a45a01eef3f
NotFiege <91145413+NotFiege@users.noreply.github.com>
2024-07-22 16:16:54 -0500

    Create letter.cpp

---
+++ letter.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() 
+{
+  std::cout << "Dear Self,\n";
+  std::cout << "You completed CS50 which was a long endeavor but it was worth it. The foundational knowledge gained is powerful and it will finally be time to reap the rewards of your efforts.\n";
+}  
