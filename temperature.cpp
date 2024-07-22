5c6220481eabb2cd4bf3bd56b320768623e60aae
NotFiege <91145413+NotFiege@users.noreply.github.com>
2024-07-22 16:49:23 -0500

    Update temperature.cpp

--- temperature.cpp
+++ temperature.cpp
@@ -4,11 +4,15 @@
 
 int main() 
 {
-  
-  double tempf = 79;
+
+  double tempf;
   double tempc;
+
+  std::cout << "Enter the temperature in Fahrenheit: ";
+  std::cin >> tempf;
 
   tempc = (tempf - 32) / 1.8;
   
   std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+  
 }
